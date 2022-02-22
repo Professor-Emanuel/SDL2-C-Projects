3e1f79c94b3abc54de9b35aae9232e3e9982b2f0
Professor-Emanuel <87651738+Professor-Emanuel@users.noreply.github.com>
2022-02-22 14:52:51 +0200

    Add files via upload

---
+++ 2D_GameEngine - Textures/2D_GameEngine/Game.h
@@ -0,0 +1,30 @@
+//#pragma once
+
+#ifndef Game_h
+#define Game_h
+
+#include "SDL.h"
+#include "SDL_image.h"
+#include <stdio.h>
+#include <iostream>
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	void initialize(const char* title, int xpos, int ypos, int width, int height, bool fullScreen);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+	bool running() { return isRunning; }
+
+private:
+	int counter = 0;
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
+
+#endif; /* Game_h */
---
+++ 2D_GameEngine - Textures/2D_GameEngine/main.cpp
@@ -0,0 +1,27 @@
+//***
+//don't forget to copy-paste SDL2.dll in the same folder as main.cpp
+//the project simply creates a white window, but we use OOP in comparison with FirstSDLWindow project
+/*
+* As long as the game is running: 
+1. handle any user input
+2. update all objects eg positions etc.
+3. render changes to the display
+*/
+
+#include "Game.h"
+
+Game* game = nullptr;
+//const char* argv[] -> the const generates some errors
+int main(int argc, char* argv[]) {
+	game = new Game();
+	game->initialize("GameEngine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+	while (game->running()) {
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+
+	game->clean();
+	return 0;
+}
